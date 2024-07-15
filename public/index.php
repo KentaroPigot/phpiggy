eea21a633040a8771327cc51b10995a7a538e7c0
KentaroPigot <56950222+KentaroPigot@users.noreply.github.com>
2024-07-15 10:21:37 +0200

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,12 @@
+
+
+<?php
+// Responsable de l'initialization. Rien d'autre 
+
+// Pour setup
+// ini_set('memory_limit', "255M");
+// echo ini_get("memory_limit");
+
+$app = include __DIR__ . "/../src/App/bootstrap.php";
+
+$app->run();
---
+++ src/App/bootstrap.php
@@ -0,0 +1,14 @@
+<?php  
+
+// Reponsable de charger les autres fichier et configurer le projet
+// Attention, on call pas la method run de l'instance App. Démarrer l'appli c'est le taff de index.php 
+
+
+declare(strict_types=1);
+
+require __DIR__ . "/../../vendor/autoload.php";
+
+use Framework\App;
+
+$app = new App();
+return $app;
