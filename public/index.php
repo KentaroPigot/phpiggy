f378e0b925ea1df20d81b3a01268217990c10dcc
KentaroPigot <56950222+KentaroPigot@users.noreply.github.com>
2024-07-19 00:50:45 +0200

    Started adding routing

--- public/index.php
+++ public/index.php
@@ -7,6 +7,8 @@
 // ini_set('memory_limit', "255M");
 // echo ini_get("memory_limit");
 
+include __DIR__ . "/../src/App/functions.php";
+
 $app = include __DIR__ . "/../src/App/bootstrap.php";
 
 $app->run();
