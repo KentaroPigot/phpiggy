fc8c058f25b7879d7d720c9f798500d35d324bd0
KentaroPigot <56950222+KentaroPigot@users.noreply.github.com>
2024-07-25 15:24:27 +0200

    Added Dependency injection, view template, templateEngine, and paths folder

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 
 // Reponsable de charger les autres fichier et configurer le projet
 // Attention, on call pas la method run de l'instance App. Démarrer l'appli c'est le taff de index.php 
@@ -9,11 +9,12 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
-use App\Controllers\HomeController;
+use App\Config\Paths;
 
+use function App\Config\registerRoutes;
 
-$app = new App();
+$app = new App(Paths::SOURCE . "App/container-definitions.php");
 
-$app->get('/', [HomeController::class, 'home']);
+registerRoutes($app);
 
 return $app;
---
+++ src/Framework/Container.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework;
+
+use ReflectionClass, ReflectionNamedType;
+use Framework\Exceptions\ContainerException;
+
+class Container
+{
+    private array $definitions = [];
+
+    public function addDefinitions(array $newDefinitions)
+    {
+        $this->definitions = [...$this->definitions, ...$newDefinitions];
+    }
+
+    public function resolve(string $className)
+    {
+        $reflectionClass = new ReflectionClass($className);
+
+        if (!$reflectionClass->isInstantiable()) {
+            throw new ContainerException("Class {$className} is not instantiable");
+        }
+
+        $constructor = $reflectionClass->getConstructor();
+
+        // une class peut ne pas avoir de constructor
+        if (!$constructor) {
+            return new $className;
+        }
+
+        $params = $constructor->getParameters();
+
+        if (count($params) === 0) {
+            return new $className;
+        }
+
+        $dependencies = [];
+
+        foreach ($params as $param) {
+            $name = $param->getName();
+            $type = $param->getType();
+
+            // On veut absolument que le paramètre du constructor ait un type. 
+            if (!$type) {
+                throw new ContainerException("Failed to resolve class {$className} because param {$name} is missing a type hint.");
+            }
+
+            // isBuiltIn() permet de vérifier si la variable est d'un des type builtin de php (bool, string, num). Class n'est pas un builtin.
+            if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
+                throw new ContainerException("Failed to resolve class {$className} because invalid param name.");
+            }
+
+            $dependencies[] = $this->get($type->getName());
+        }
+
+        // newInstanceArgs permet d'instantier la class qui est reflected, avec des arguments
+        return $reflectionClass->newInstanceArgs($dependencies);
+    }
+
+    public function get(string $id)
+    {
+        if (!array_key_exists($id, $this->definitions)) {
+            throw new ContainerException("Class {$id} does not exist in container.");
+        }
+
+        $factory = $this->definitions[$id];
+
+        $dependency = $factory();
+
+        return $dependency;
+    }
+}
